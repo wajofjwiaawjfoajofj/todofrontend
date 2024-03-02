3fc134ff60409f35a8987509529499a77390300f
shunma <shunmakajioka12@gmail.com>
2024-03-02 20:09:56 +0900

    apiの準備

---
+++ src/API.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+//バックエンド側のURL
+const URL_API = 'http://localhost:3000';
+
+//Todo一覧を表示するAPIを叩く
+ const GetTodos  = async () => {
+    try{
+        const res = await axios.get(`${URL_API}/todos`)
+        return res.data;
+    }
+    catch(error)
+    {
+        console.log('エラーが発生しました' ,error)
+        throw error;
+    }
+}
+
+export default 
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GetTodos } from '../api';
 
 const List = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const TodoList = async () => {
+            try{
+                const getFromAPI = await GetTodos();
+                setTodos(getFromAPI);
+            }
+            catch(error){
+                console.log('エラーが発生しました', error)
+            }
+        }
+        TodoList();
+    },[])
+
+
+
   return (
-    <div>List</div>
+    <div>
+        <h1>Todoリスト</h1>
+        <ul>
+        {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+        ))}
+        </ul>
+    </div>
   )
 }
 
