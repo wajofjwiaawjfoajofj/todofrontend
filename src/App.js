7f6692fd350edddfdeabb0757d3c7b3c971bcdf7
shunma <shunmakajioka12@gmail.com>
2024-03-02 15:47:27 +0900

    routerの追加とapp.cssのレイアウト

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Complete from './Components/Complete';
 import Form from './Components/Form';
 import List from './Components/List';
 import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 
@@ -11,11 +12,21 @@ import Sidebar from './Sidebar';
 function App() {
   return (
     <>
+    <Router>
+    <div className="wrapper">
+    <div className='leftside'> 
     <Sidebar/>
-    <Form/>
-    <List/>
-    <Complete/>
-    <About/>
+    </div>
+    <div className='rightside'>
+    <Routes>
+    <Route path='/' element={<Form/>} />
+    <Route path='/list' element={<List/>} />
+    <Route path='/comolete' element={<Complete/>} />
+    <Route path='/about' element={<About/>} />
+    </Routes>
+    </div>
+    </div>
+    </Router>
     </>
   );
 }
