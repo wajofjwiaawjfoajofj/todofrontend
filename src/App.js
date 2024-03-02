1b97b233a9a60d66eb46e237e23309f47b275e1b
shunma <shunmakajioka12@gmail.com>
2024-03-02 15:19:01 +0900

    必要なコンポーネントを揃える

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
+import Complete from './Components/Complete';
+import Form from './Components/Form';
+import List from './Components/List';
+import Sidebar from './Sidebar';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Sidebar/>
+    <Form/>
+    <List/>
+    <Complete/>
+    <About/>
+    </>
   );
 }
 
