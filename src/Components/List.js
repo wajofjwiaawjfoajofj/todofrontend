7bfb36ea00ae304c8dc996fb9a94b4b75734f7cd
shunma <shunmakajioka12@gmail.com>
2024-03-02 21:59:29 +0900

    dummyデータの追加

--- src/Components/List.js
+++ src/Components/List.js
@@ -56,23 +56,47 @@
 
 
 
+
+
+
+//↓ダミーデータ用
+
+
 import React, { useEffect, useState } from 'react';
 import TodoListDummy from '../Dummy/dummy';
+import { TodoListDummy2 } from '../Dummy/dummy';
 
 const List = () => {
     const [todos, setTodos] = useState([]);
+    const [todos2, setTodo2] = useState([]);
 
     useEffect(() => {
         setTodos([TodoListDummy]);
     }, []);
 
+    useEffect(() => {
+        setTodo2([TodoListDummy2]);
+    }, []);
+
+
+
     return (
         <div>
-            <h1>Todoリスト</h1>
+            <h1>リスト</h1>
             <h2>Udemy</h2>
             <ul>
                 {todos.map((todo) => {
                     if (todo.type === 'udemy') {
+                        return <li key={todo.id}>{todo.title}{todo.type}</li>;
+                    } else {
+                        return null;
+                    }
+                })}
+            </ul>
+            <h2>Paiza</h2>
+            <ul>
+                {todos2.map((todo) => {
+                    if (todo.type === 'paiza') {
                         return <li key={todo.id}>{todo.title}{todo.type}</li>;
                     } else {
                         return null;
--- src/Dummy/dummy.js
+++ src/Dummy/dummy.js
@@ -4,3 +4,12 @@ const TodoListDummy = {
 }
 
 export default TodoListDummy;
+
+
+
+export const TodoListDummy2 = {
+    title: "関口paizaAランク",
+    type: 'paiza'
+}
+
+
