7d1f0d096ddd822b2405da8a1ac2d9d26d0efcab
shunma <shunmakajioka12@gmail.com>
2024-03-04 01:37:45 +0900

    pc版のlistsideを整える

--- src/Components/SideList.js
+++ src/Components/SideList.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './SideList.css'
 
 const SideList = () => {
   return (
     <>
-     <Link to="/">タスクの追加</Link>
-     <Link to="/list">実行中のtodo</Link>
-     <Link to="/complete">完了済みのTodo</Link>
-     <Link to="/about">このアプリについて</Link>
+    <div className='sideBarwrapper'>
+     <Link to="/" className='Link'>タスクの追加</Link>
+     <Link to="/list" className='Link'>実行中のtodo</Link>
+     <Link to="/complete" className='Link'>完了済みのTodo</Link>
+     <Link to="/about" className='Link'>このアプリについて</Link>
+     </div>
     </>
   )
 }
