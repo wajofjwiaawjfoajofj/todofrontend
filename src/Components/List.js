f2f1a80c97968e6837191229da4e934466aed6d7
shunma <shunmakajioka12@gmail.com>
2024-03-04 22:42:22 +0900

    listの体裁を整える

--- src/Components/List.js
+++ src/Components/List.js
@@ -65,11 +65,16 @@
 import React, { useEffect, useState } from 'react';
 import TodoListDummy from '../Dummy/dummy';
 import { TodoListDummy2 } from '../Dummy/dummy';
+import { Button } from '@mui/material';
+import './List.css';
 
 const List = () => {
+
+    //ダミーデータ
     const [todos, setTodos] = useState([]);
     const [todos2, setTodo2] = useState([]);
 
+ 
     useEffect(() => {
         setTodos([TodoListDummy]);
     }, []);
@@ -81,13 +86,21 @@ const List = () => {
 
 
     return (
-        <div>
-            <h1>リスト</h1>
+        <div className='Listmain'>
+            <div className='Listwrapper'>
             <h2>Udemy</h2>
             <ul>
                 {todos.map((todo) => {
                     if (todo.type === 'udemy') {
-                        return <li key={todo.id}>{todo.title}{todo.type}</li>;
+                        return <li key={todo.id}>
+                            <p>{todo.title}</p>
+                            <div className='inner'>
+                            {`残り${todo.date}日`}
+                            <Button variant='outlined'>完了</Button>
+                            <Button  variant='outlined'  color='warning'>削除</Button>
+                            <Button   variant='outlined'  color='info'>編集</Button>
+                            </div>
+                            </li>;
                     } else {
                         return null;
                     }
@@ -97,12 +110,21 @@ const List = () => {
             <ul>
                 {todos2.map((todo) => {
                     if (todo.type === 'paiza') {
-                        return <li key={todo.id}>{todo.title}{todo.type}</li>;
+                        return <li key={todo.id}>
+                        <p>{todo.title}</p>
+                        <div className='inner'>
+                        {`残り${todo.date}日`}
+                        <Button variant='outlined'>完了</Button>
+                        <Button  variant='outlined'  color='warning'>削除</Button>
+                        <Button   variant='outlined'  color='info'>編集</Button>
+                        </div>
+                        </li>;
                     } else {
                         return null;
                     }
                 })}
             </ul>
+            </div>
         </div>
     );
 }
--- src/Dummy/dummy.js
+++ src/Dummy/dummy.js
@@ -1,6 +1,7 @@
 const TodoListDummy = {
-    title: 'Udemy',
-    type: 'udemy'    
+    title: '今後のフロントエンド開発で必須となるReact v18の機能を丁寧に解説する',
+    type: 'udemy' ,
+    date: '6'   
 }
 
 export default TodoListDummy;
@@ -9,7 +10,8 @@ export default TodoListDummy;
 
 export const TodoListDummy2 = {
     title: "関口paizaAランク",
-    type: 'paiza'
+    type: 'paiza',
+    date: '15'
 }
 
 
